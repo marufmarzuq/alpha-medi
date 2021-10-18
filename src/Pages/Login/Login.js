5b29e629d832006b2546c18c1e2490b7e742417f
marufmarzuq <marufmarzuq18@gmail.com>
2021-10-19 02:14:41 +0600

    login and register page design complete

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
+import Header from '../../Shared/Header/Header';
+import "./Login.css"
 const Login = () => {
      return (
-          <div>
-               login
-          </div>
+          <>
+               <Header></Header>
+               <div className="login">
+                    <div className="container d-flex justify-content-center">
+                         <div className="form-container">
+                         <form>
+                              <div className="mb-3">
+                              <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                              </div>
+                              <div className="mb-3">
+                              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                              <input type="password" className="form-control" id="exampleInputPassword1"/>
+                              </div>
+                              <button type="submit" className="btn btn-dark">Log in</button>
+                              </form>
+                              <hr />
+                              <p className="text-center">or</p>
+                              <button type="submit" className="btn btn-outline-dark w-100">Log in with Gmail</button>
+                              <p className="pt-4">Don’t have an account yet? <Link to="/register">register</Link></p>
+                         </div>
+                    </div>
+               </div>
+               <Footer></Footer>
+          </>
      );
 };
 
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
+import Header from '../../Shared/Header/Header';
 
 const Register = () => {
      return (
-          <div>
-               register
+          <>
+          <Header></Header>
+          <div className="login">
+               <div className="container d-flex justify-content-center">
+                    <div className="form-container">
+                    <form>
+                         <div className="mb-3">
+                         <label htmlFor="fullname" className="form-label">Full Name</label>
+                         <input type="text" className="form-control" id="fullname" aria-describedby="emailHelp"/>
+                         </div>
+                         <div className="mb-3">
+                         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                         </div>
+                         <div className="mb-3">
+                         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                         <input type="password" className="form-control" id="exampleInputPassword1"/>
+                         </div>
+                         <button type="submit" className="btn btn-dark">Register</button>
+                         </form>
+                         <hr />
+                         <p className="pt-4">Already have an account? <Link to="/login">Log in</Link></p>
+                    </div>
+               </div>
           </div>
+          <Footer></Footer>
+     </>
      );
 };
 
