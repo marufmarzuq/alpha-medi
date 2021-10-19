00f7e603c4d98e5ccb7e1478237342fa28d30d6c
marufmarzuq <marufmarzuq18@gmail.com>
2021-10-19 22:10:04 +0600

    review finished

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
           <header>
                <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
   <div className="container">
-    <a className="navbar-brand" href="/"><img className="app-logo" src={logo} alt="" /></a>
+    <Link className="navbar-brand" to="/"><img className="app-logo" src={logo} alt="" /></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
